43c17329c0aa8a8f020f8cbf97c3ce0648e0e41b
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2023-10-12 20:25:53 +0530

    Initial Backdrop Shadow Completed

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
+    const shadows =  useRef();
     return (
-    <div>Backdrop</div>
+        <AccumulativeShadows
+            ref={shadows}
+            temporal
+            frames={60}
+            alphaTest={0.15}
+            scale={10}
+            rotation={[Math.PI / 2, 0, 0]}
+            position={[0, 0, -0.4]}>
+
+            <RandomizedLight 
+                amount={2}
+                radius={9}
+                intensity={0.35}
+                ambient={0.25}
+                position={[5, 5, -10]}
+                />
+            <RandomizedLight 
+                amount={4}
+                radius={5}
+                intensity={0.25}
+                ambient={0.35}
+                position={[-6, 5, -9]}
+                />
+        </AccumulativeShadows>
     )
 }
 
