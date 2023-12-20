9b0e0e4a75db21c870d81b28ab8acbb49a02be44
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2023-12-20 12:58:34 +0530

    open ai (DALL-E) integration for prompts

--- client/src/components/aiPicker.jsx
+++ client/src/components/aiPicker.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
-const AiPicker = () => {
+import CusButtn from './cusButtn'
+
+const AiPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
     return (
-        <div>AiPicker</div>
+        <div className='aipicker-container'>
+            <textarea 
+                placeholder='Enter your own Prompt..'
+                rows={4}
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                className='aipicker-textarea'
+            />
+            <div className='flex flex-wrap gap-3'>
+                {generatingImg ? (
+                    <CusButtn
+                        type="outline"
+                        title="Asking AI"
+                        customStyles="text-xs"
+                    />
+
+                ) : (
+                    <>
+                        <CusButtn
+                            type="outline"
+                            title="AI Logo"
+                            handleClick={() => handleSubmit('logo')}
+                            customStyles="text-xs"
+                        />
+                        
+                        <CusButtn
+                            type="filled"
+                            title="AI Full"
+                            handleClick={() => handleSubmit('full')}
+                            customStyles="text-xs"
+                        />
+                    </>
+                )
+            }
+            </div>
+        </div>
     )
 }
 
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,15 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi} from "openai";
+import OpenAI from "openai";
 
 dotenv.config();
 
 const router  = express.Router();
 
-const config = new Configuration({
-    apiKey: process.env.DALLE-API,
-});
+const openai = new OpenAI({
+    apiKey : process.env.DALLE_API
+})
 
-const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
     res.status(200).json({ message: "All OpenAI Routes Accessed" })
@@ -20,13 +19,14 @@ router.route('/').post(async (req, res) => {
     try{
         const { prompt } = req.body;
 
-        const response = openai.createImage({
-            prompt, n: 1,
+        const response = await openai.images.generate({
+            prompt, 
+            n: 1,
             size: "1024x1024",
             response_format: 'b64_json'
         });
-
-        const image = response.data.data[0].b64_json;
+        
+        const image = response.data[0].b64_json;
 
         res.status(200).json({ photo: image });
 
